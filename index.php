4386a4528a2c895685c140d7337ec63d2144cf31
Lorenzo Magro <magrolorenzo@gmail.com>
2020-12-16 18:30:44 +0100

    Stilati link nei paragrafi

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ Una volta che siamo riusciti ad ottenere un buon risultato con array e html+php 
         [
             "question" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
             "answer" => [
-                "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa.
+                "La recente <a>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa.
                 La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome.
                 Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
 
@@ -20,7 +20,7 @@ Una volta che siamo riusciti ad ottenere un buon risultato con array e html+php 
                 Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali
                 ed il diritto di tutti di conoscere e distribuire le informazioni.",
 
-                "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta.
+                "Per presentare una richiesta di rimozione, compila questo <a>modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta.
                 Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili).
                 Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata.
                 Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche,
@@ -49,9 +49,9 @@ Una volta che siamo riusciti ad ottenere un buon risultato con array e html+php 
                 Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci.
                 Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
 
-                "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.",
+                "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a>Centro Google per la sicurezza online.</a>",
 
-                "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
+                "<a>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
             ]
         ],
         // FAQ 3
@@ -60,10 +60,10 @@ Una volta che siamo riusciti ad ottenere un buon risultato con array e html+php 
             "answer" => [
                 "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web.
                 I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web.
-                Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
-                Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui.
+                Se desideri rimuovere qualcosa dal Web, devi <a>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica.
+                Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a>fai clic qui</a>.
                 Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google.
-                In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
+                In caso di una richiesta di rimozione urgente, è inoltre possibile <a>visitare la nostra pagina di assistenza per avere ulteriori informazioni.</a>"
             ]
         ],
         // FAQ 4
@@ -71,11 +71,11 @@ Una volta che siamo riusciti ad ottenere un buon risultato con array e html+php 
             "question" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
             "answer" => [
                 "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet,
-                o URL, della pagina dei risultati di ricerca sotto forma di URL referrer.
+                o URL, della pagina dei risultati di ricerca sotto forma di <a>URL referrer</a>.
                 Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito.
                 Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer.
                 Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi.
-                Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics
+                Ulteriori informazioni sulla ricerca SSL sono disponibili <a>qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics
                 o un'API (Application Programming Interface).
                 Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
             ]
